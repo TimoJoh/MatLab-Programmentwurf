a74b6c77ebc40e108f2c098662e40d41aa60a992
TimoPrivat <timo.sebastian.johannsen@gmail.com>
2025-04-20 13:22:38 +0200

    Aufgabe B

--- Code/compute_ljapunov_exponents.m
+++ Code/compute_ljapunov_exponents.m
@@ -10,27 +10,31 @@ for idx = 1:length(K_vals)
     I = rand()*2*pi;
     theta = rand()*2*pi;
     Q = eye(2);
-    sum_log_diag = zeros(1,2);
+
+    sumLogR11 = 0;
+    sumLogR22 = 0;
 
     for n = 1:N
-        I = mod(I + K*sin(theta), 2*pi);
-        theta = mod(theta + I, 2*pi);
         % Ableitungsmatrix DF
         DF = [1, K*cos(theta); 1, 1 + K*cos(theta)];
         A = DF * Q;
         [Q, R] = qr(A);
-        sum_log_diag = sum_log_diag + log(abs(diag(R))');
-    end
+        sumLogR11 = sumLogR11 + log(abs(R(1,1)));
+        sumLogR22 = sumLogR22 + log(abs(R(2,2)));
 
-    lambda1(idx) = sum_log_diag(1)/N;
-    lambda2(idx) = sum_log_diag(2)/N;
+        I = mod(I + K*sin(theta), 2*pi);
+        theta = mod(theta + I, 2*pi);
+    end
+    
+    lambda1(idx) = sumLogR11 / N;
+    lambda2(idx) = sumLogR22 / N;
 end
 
 figure;
-plot(K_vals, lambda1, 'r', K_vals, lambda2, 'b');
+plot(K_vals, lambda1, '-r', K_vals, lambda2, '-b');
 xlabel('K');
 ylabel('Ljapunov-Exponenten');
-legend('\lambda_1', '\lambda_2');
+legend('\lambda_1', '\lambda_2', 'Location','northwest');
 title('Ljapunov-Exponenten in Abhängigkeit von K');
 grid on;
 
